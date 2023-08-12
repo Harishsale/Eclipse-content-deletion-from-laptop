359f55f4a49567e632185aa4ad070feb075eab91
Jakkam Sairam <Jakkam Sairam@DESKTOP-1KMDQFU>
2023-08-13 00:44:36 +0530

    update

---
+++ src/main/java/com/hospiol/pageobjects/Homepage.java
@@ -0,0 +1,7 @@
+package com.hospiol.pageobjects;
+
+import com.hospiol.base.TestBase;
+
+public class Homepage extends TestBase{
+
+}
--- src/main/java/com/hospiol/pageobjects/Loginpage.java
+++ src/main/java/com/hospiol/pageobjects/Loginpage.java
@@ -1,5 +1,6 @@
 package com.hospiol.pageobjects;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -7,9 +8,6 @@ import org.openqa.selenium.support.PageFactory;
 import com.hospiol.base.TestBase;
 
 public class Loginpage extends TestBase {
-
-	@FindBy(xpath = "(//a[text()='Admin login '])[1]")
-	WebElement AdminLogin;
 
 	@FindBy(name = "username")
 	WebElement username;
@@ -20,14 +18,12 @@ public class Loginpage extends TestBase {
 	@FindBy(xpath = "//button[@class='btn']")
 	WebElement buttonClick;
 
-	public Loginpage() {
-
+	public Loginpage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
 
 	public Homepage Verifylogin() {
 
-		AdminLogin.click();
 		username.sendKeys(prop.getProperty("username"));
 		password.sendKeys(prop.getProperty("password"));
 		buttonClick.click();
@@ -35,10 +31,9 @@ public class Loginpage extends TestBase {
 		return new Homepage();
 	}
 
-	public String Urltest() {
-
-		String applicationurl = driver.getCurrentUrl();
-		return applicationurl;
-	}
+//	public String Urltest() {
+//
+//	return driver.getCurrentUrl();
+//	}
 
 }
